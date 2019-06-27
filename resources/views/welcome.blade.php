0fa3b6270e4d84aa624e82e8a605ad25f7230bac
Lyle <Lylemecaskey3@gmail.com>
2019-06-27 12:08:59 -0500

    Reformat and Title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,59 +1,66 @@
 @extends('layouts.master')
 
 @section('content')
+    {{-- Function that turns XP into Levels --}}
+    <?php
+    function getLevel($xp)
+    {
+        $levels = array(
+            0, 0, 83, 174, 276, 388, 512, 650, 801, 969, 1154, 1358, 1584, 1833, 2107, 2411, 2746, 3115, 3523, 3973,
+            4470, 5018, 5624, 6291, 7028, 7842, 8740, 9730, 10824, 12031, 13363, 14833, 16456, 18247, 20224, 22406,
+            24815, 27473, 30408, 33648, 37224, 41171, 45529, 50339, 55649, 61512, 67983, 75127, 83014, 91721, 101333,
+            111945, 123660, 136594, 150872, 166636, 184040, 203254, 224466, 247886, 273742, 302288, 333804, 368599,
+            407015, 449428, 496254, 547953, 605032, 668051, 737627, 814445, 899257, 992895, 1096278, 1210421, 1336443,
+            1475581, 1629200, 1798808, 1986068, 2192818, 2421087, 2673114, 2951373, 3258594, 3597792, 3972294, 4385776,
+            4842295, 5346332, 5902831, 6517253, 7195629, 7944614, 8771558, 9684577, 10692629, 11805606, 13034431
+        );
 
-<?php
-function getLevel($xp)
-{
-    $levels = array(
-        0, 0, 83, 174, 276, 388, 512, 650, 801, 969, 1154, 1358, 1584, 1833, 2107, 2411, 2746, 3115, 3523, 3973,
-        4470, 5018, 5624, 6291, 7028, 7842, 8740, 9730, 10824, 12031, 13363, 14833, 16456, 18247, 20224, 22406,
-        24815, 27473, 30408, 33648, 37224, 41171, 45529, 50339, 55649, 61512, 67983, 75127, 83014, 91721, 101333,
-        111945, 123660, 136594, 150872, 166636, 184040, 203254, 224466, 247886, 273742, 302288, 333804, 368599,
-        407015, 449428, 496254, 547953, 605032, 668051, 737627, 814445, 899257, 992895, 1096278, 1210421, 1336443,
-        1475581, 1629200, 1798808, 1986068, 2192818, 2421087, 2673114, 2951373, 3258594, 3597792, 3972294, 4385776,
-        4842295, 5346332, 5902831, 6517253, 7195629, 7944614, 8771558, 9684577, 10692629, 11805606, 13034431
-    );
-
-    foreach($levels as $x => $x_value) {
-        if($x_value >= $xp){
-            return $x;
+        foreach ($levels as $x => $x_value) {
+            if ($x_value >= $xp) {
+                return $x;
+            }
         }
+        return $x;
     }
-    return $x;
-}
+    ?>
 
-?>
+    <link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
 
-{{--CHARACTER SEARCH BOX--}}
-<link href="https://fonts.googleapis.com/css?family=VT323&display=swap" rel="stylesheet">
-<style>
-    .skill-table {
-        padding: 0.25rem;
-    }
-    .card-img-top {
-        max-height: 75px;
-        min-width: 250px;
+    <style>
+        .skill-table {
+            padding: 0.25rem;
+        }
 
-    }
-    .card-img-overlay{
-        max-height: 75px;
-        min-width: 250px;
-        background-color: #8b7860;
-    }
-    .card{
-        min-width: 250px;
-    }
-    .osrs-text{
-        font-family: 'VT323', monospace;
-        color: palegoldenrod;
-        text-shadow: 2px 1px #1d2124;
-    }
-    .pagination {
-        align-self: flex-end;
-        padding-right: 1rem;
-    }
-</style>
+        .card-img-top {
+            max-height: 75px;
+            min-width: 250px;
+
+        }
+
+        .card-img-overlay {
+            max-height: 75px;
+            min-width: 250px;
+            background-color: #8b7860;
+        }
+
+        .card {
+            min-width: 250px;
+        }
+
+        .osrs-text {
+            font-family: 'VT323', monospace;
+            color: palegoldenrod;
+            text-shadow: 2px 1px #1d2124;
+        }
+
+        .pagination {
+            align-self: flex-end;
+            padding-right: 1rem;
+        }
+    </style>
+
+@section('title', 'Dashboard')
+
 <div class="content-wrapper">
     <div class="row">
         <div class="card-deck ml-auto mr-auto col-12">
@@ -63,11 +70,12 @@ function getLevel($xp)
                     <h2 class="text-center align-middle m-0 osrs-text">TRACK CHARACTER</h2>
                 </div>
                 <div class="card-body">
-                    <form  action="/highscores" method="POST">
+                    <form action="/highscores" method="POST">
                         {{ csrf_field() }}
                         <div class="form-group">
                             <label for="player">Character</label>
-                            <input type="text" class="form-control" name="user_input" id="player" placeholder="Character Name..." >
+                            <input type="text" class="form-control" name="user_input" id="player"
+                                   placeholder="Character Name...">
                         </div>
                         <button class="btn btn-primary" type="submit">Search</button>
                     </form>
@@ -80,8 +88,11 @@ function getLevel($xp)
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to
+                        additional content. This content is a little bit longer.</p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
                 </div>
             </div>
             <div class="card">
@@ -91,8 +102,11 @@ function getLevel($xp)
                 </div>
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional
+                        content.</p>
+                    <p class="card-text">
+                        <small class="text-muted">Last updated 3 mins ago</small>
+                    </p>
                 </div>
             </div>
         </div>
@@ -131,46 +145,38 @@ function getLevel($xp)
                     <tbody>
                         @foreach ($highscores as $highscore)
                             <tr>
-                                    <td class="skill-table" scoped='row'>{{$highscore->Character_Name}}</td>
-                                    <td class="skill-table" scoped='row'>{{$highscore->Overall_XP}}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Attack_XP)  }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Defence_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Strength_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Hitpoints_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Ranged_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Prayer_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Magic_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Cooking_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Woodcutting_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Fletching_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Fishing_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Firemaking_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Crafting_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Smithing_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Mining_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Herblore_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Agility_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Thieving_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Slayer_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Farming_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Runecrafting_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Hunter_XP) }}</td>
-                                    <td class="skill-table">{{ getLevel($highscore->Construction_XP) }}</td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                                <td class="skill-table" scoped='row'>{{$highscore->Character_Name}}</td>
+                                <td class="skill-table" scoped='row'>{{$highscore->Overall_XP}}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Attack_XP)  }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Defence_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Strength_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Hitpoints_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Ranged_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Prayer_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Magic_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Cooking_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Woodcutting_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Fletching_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Fishing_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Firemaking_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Crafting_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Smithing_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Mining_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Herblore_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Agility_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Thieving_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Slayer_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Farming_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Runecrafting_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Hunter_XP) }}</td>
+                                <td class="skill-table">{{ getLevel($highscore->Construction_XP) }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
                 {{ $highscores->links() }}
             </div>
         </div>
     </div>
 </div>
-
-<script>
-
-    document.getElementById('level').innerHtml = "3";
-
-
-</script>
-
 @endsection
